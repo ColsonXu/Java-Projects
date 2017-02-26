3f872f6d2ebf3d7e46ceb316fce13b4b62a425bb
ColsonXu <colson.xu@gmail.com>
2017-02-26 19:08:19 +0800

    upload the newest version of everything.

--- src/class/CarSelector.java
+++ src/class/CarSelector.java
@@ -1,6 +1,5 @@
 // Car Selector
 
-package Classes;
 import java.util.Scanner;
 
 public class CarSelector {	
@@ -14,35 +13,34 @@ public class CarSelector {
 		Cars Honda = new Cars("Honda", "SUV", 'C', 4, 70500.0);
 		Cars Volkswagen = new Cars("Volkswagen", "Vintage", 'C', 2, 130000.0);
 //***********************************************************************		
-		
-		System.out.println(BMW.getModel() + Benz.getModel());
-		Scanner typeIn = new Scanner(System.in);
-		System.out.println("What kind of car do you want?\n"
-				+ "  Jeep\n"
-				+ "  Limousine\n"
-				+ "  Supercar\n"
-				+ "  SUV\n"
-				+ "  Vintage");
-		String species = typeIn.next();
-		if (species.equals("Jeep")) {
-			System.out.println("We have a Jeep for you!");
-		} else if (species.equals("Limousine")) {
-			System.out.println("We have a Limousine for you!");
-		} else if (species.equals("Supercar")) {
-			System.out.println("The supercar is ready for you to ride.");
-		} else if (species.equals("SUV")) {
-			System.out.println("Take your family out with your new SUV this week!");
-		} else if (species.equals("Vintage")) {
-			System.out.println("Special car for the special one.");
-		} else {
-			System.out.println("Please make sure you enter the right word.");
+
+
+		while (true) {
+			Scanner typeIn = new Scanner(System.in);
+			System.out.println("What kind of car do you want?\n"
+					+ "  Jeep\n"
+					+ "  Limousine\n"
+					+ "  Supercar\n"
+					+ "  SUV\n"
+					+ "  Vintage");
+			String species = typeIn.next();
+			if (species.equals("Jeep") || species.equals("Limousine") || species.equals("Supercar") || species.equals("SUV") || species.equals("Vintage")) {
+				System.out.println(species);
+				typeIn.close();
+				break;
+			} else {
+				System.out.println("Please make sure you enter the right type.");
+				typeIn.close();
+			}
 		}
-		typeIn.close();
 		
-		Scanner rangeIn = new Scanner(System.in);
-		System.out.println("From A to C as it ranking from high to low. What price range would you preffer?");
-		char range = (char) rangeIn.nextByte();
 		
+			Scanner rangeIn = new Scanner(System.in);
+			System.out.println("From A to C as it ranking from high to low. What price range would you preffer?");
+			char range = (char) rangeIn.nextByte();
+		
+		}
+
 	}
 
 }
