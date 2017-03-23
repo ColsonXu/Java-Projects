57c8376792f0c62af810ebfba196fe286a3bd2aa
Colson Xu <Colson Xu>
2017-03-23 10:02:06 +0800

    Systematic Update

--- src/Scratch/s1.java
+++ src/Scratch/s1.java
@@ -1,7 +1,6 @@
 package Scratch;
 
 import java.*;
-
 import Tutorials.printArray;
 
 public class s1 {
@@ -10,13 +9,10 @@ public class s1 {
 	public static void main(String[] args) {
 // **********************************************************
 		
-		
-			Integer num1 = new Integer(34567);
-			System.out.println(num1.valueOf(2));
+		for (int i = 0; i < 20; i++) {
+			System.out.println(Math.random());
+		}
 
-		
-		
-		
 // **********************************************************
 	}
 	
