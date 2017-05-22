54c8fa48ff0d69a84198aafc62a0ecf3314574b7
ColsonXu <colson.xu@gmail.com>
2017-05-22 09:56:39 +0800

    Systematic Update

--- src/Scratch/s1.java
+++ src/Scratch/s1.java
@@ -8,8 +8,12 @@ public class s1 {
 
 	public static void main(String[] args) {
 // **********************************************************
-		char[] mString = {'h','e','l','l','o'};
-		String s1 = new String(mString);
+		String s1 = new String("Greetings");
+		String s2 = new String("Greetings");
+		String s3 = s1;
+		System.out.println(s1 == s2);
+		System.out.println(s2 == s3);
+		System.out.println(s3 == s1);
 // **********************************************************
 	}
 
